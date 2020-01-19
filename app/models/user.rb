b8872a90b93bfb7b019a99ff555cb0e15d5c2191
m.kitano <11base.movie@gmail.com>
2020-01-19 13:03:38 +0000

    add model favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,16 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   
+  #フォロー機能
   has_many :microposts
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  # お気に入り機能
+  has_many :favorites
+  has_many :fav_microposts, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -29,5 +34,21 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  # お気に入り機能
+  def favorite(micropost)
+    unless self == micropost
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+  end
+  
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(micropost)
+    self.fav_microposts.include?(micropost)
   end
 end
